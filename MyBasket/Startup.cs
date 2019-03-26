0c42f992d366786386cd07c0d9d658755aba902a
Litesh Koteshawaran <44187300+LiteshKoteshwaran@users.noreply.github.com>
2019-03-26 14:35:45 +0530

    Initial Commit - 26/03 : 2:35pm

---
+++ MyBasket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBasket.Startup))]
+namespace MyBasket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
